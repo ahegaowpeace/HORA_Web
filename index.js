565359a25eb3272f5a844e43177edc4345908a5a
root <root@ip-172-31-34-209.ap-northeast-1.compute.internal>
2020-03-15 22:31:02 +0000

    Completed First UI

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'ejs');
 
@@ -17,7 +19,12 @@ app.get('/', function (req, res) {
 /*           form.ejs           */
 /********************************/
 app.get('/form', function (req, res) {
-        return res.render("form");
+	return res.render("form");
 });
+
+app.post('/form', function (req, res) {
+	return res.render("result", {data: req.body});
+});
+
 app.listen(3000, function () {
 });
